b48beda9d1e0762776bf17485d5beb2d7cbded5b
Blaine Nelson <blaine.nelson@bts-s2.com>
2014-04-09 18:34:27 -0400

    Non concurrent examples.

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
-  :main concurrent-primes.core
-  :profiles {:uberjar {:aot :all}})
+  :aot [concurrent-primes.core]
+  :main concurrent-primes.core)
+  ;; :profiles {:uberjar {:aot :all}})
--- src/concurrent_primes/core.clj
+++ src/concurrent_primes/core.clj
@@ -1,6 +1,7 @@
 (ns concurrent-primes.core
-  (:require [clojure.core.async :as async :refer [chan go <! >!]]))
- 
+  (:require [clojure.core.async :as async :refer [chan go <! >!]])
+  (:gen-class :main true))
+
 ;;; concurrent prime sieve in Clojure using core.async
 ;; inspired by a similar implementation in Go
 ;; http://golang.org/doc/play/sieve.go
@@ -53,3 +54,42 @@
           (println prime)
           (recur (inc i) nxt))))
     :ok))
+
+;; (sieve 20)
+
+(defn non-concurrent-sieve
+  ;; http://clojuredocs.org/clojure_core/clojure.core/lazy-seq#example_1000
+  ;; for reference
+  [s]
+  (cons (first s)
+        (lazy-seq (non-concurrent-sieve
+                   (filter #(pos? (rem % (first s))) (rest s))
+                                        ))))
+
+;; (take 20 (non-concurrent-sieve (iterate inc 2)))
+
+
+(defn my-generate
+  []
+  (iterate inc 2))
+  
+(defn my-sieve
+  [n]
+  (let [in (my-generate)]
+    (loop [i 0 ch in]
+      (when (< i n)
+        (let [prime (first ch)
+              nxt (filter #(pos? (rem % prime)) ch)]
+          (println prime)
+          (recur (inc i) nxt))))
+    :ok))
+
+
+(defn -main [& args]
+  (prn (format "args=%s" args))
+  (if (not (empty? args))
+    (sieve (first args))))
+    ;; (do
+    ;;   (prn "blik")
+    ;;   (sieve (first args)))))
+
